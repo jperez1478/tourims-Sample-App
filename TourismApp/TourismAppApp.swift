1fa2ca93a7e2b71c9df32c54761f09a2be46699f
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-10-12 18:52:41 -0500

    Initial Commit

---
+++ TourismApp/TourismAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourismAppApp.swift
+//  TourismApp
+//
+//  Created by Jessica Perez on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TourismAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
